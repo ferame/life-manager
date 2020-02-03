afb72b6e7c5d70d8ded37386beecc5d267770837
Justin Alisauskas <alisauskas.j@gmail.com>
2020-02-03 21:47:16 +0000

    Initial setup reworked.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../src/styles/index.css';
+
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				<h1>Life Manager</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './index.js',
-    output: {
-        filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        })
-    ],
-    resolve: {
-        modules: [__dirname, 'src', 'node_modules'],
-        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+
+  // webpack will take the files from ./src/index
+  entry: './src/index',
+
+  // and output it into /dist as bundle.js
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+
+  // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules: [
+
+        // we use babel-loader to load our jsx and tsx files
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
       },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: require.resolve('babel-loader')
-            },
-            {
-                test:/\.s?css$/,
-                use:['style-loader','css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                'file-loader'
-                ]
-            }
-        ]
-    }
-}
+
+      // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
