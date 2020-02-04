913a4fe1deac0f5a671ecee9ae489a957696b9c6
Justin Alisauskas <alisauskas.j@gmail.com>
2020-02-04 21:49:06 +0000

    Changing linting to spaces instead of tabs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 describe('Example Jest test', () => {
-    it('sums numbers', () => {
+    test('sums numbers', () => {
         expect(1 + 2).toEqual(3);
         expect(2 + 2).toEqual(4);
     });
