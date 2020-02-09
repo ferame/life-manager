7806978db46b00d89f4216f8a3ebd19cd1e2d7ae
Justin Alisauskas <alisauskas.j@gmail.com>
2020-02-09 21:32:19 +0000

    Adding styled-components and creating css for app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: #292727;
+    text-align: center;
+`;
 
 class App extends React.PureComponent {
     render() {
         return (
             <div>
-                <h1 data-testid='app-title' >Life Manager</h1>
+                <Title data-testid='app-title' >Life Manager</Title>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,6 @@ module.exports = {
           loader: 'babel-loader'
         },
       },
-
-      // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
     ]
   },
   plugins: [
