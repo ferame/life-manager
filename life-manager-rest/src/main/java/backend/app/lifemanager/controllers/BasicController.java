956329e73118077bb3a111caaaef19ec1f81ca98
Justinas Alisauskas <alisauskas.j@gmail.com>
2020-11-24 22:11:05 +0000

    Super basic initial setup of rest api

---
+++ life-manager-rest/src/main/java/backend/app/lifemanager/controllers/BasicController.java
@@ -0,0 +1,18 @@
+package backend.app.lifemanager.controllers;
+
+import backend.app.lifemanager.dao.BasicResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class BasicController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/basic")
+    public BasicResponse basicEndpoint(@RequestParam(value = "text", defaultValue = "Default text") String text) {
+        return new BasicResponse(counter.incrementAndGet(), text);
+    }
+}
