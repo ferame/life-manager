8002a3ef829cc6d075e89d6768578a683c526604
ferame <alisauskas.j@gmail.com>
2020-12-15 21:59:59 +0000

    Adding stylesheet

--- life-manager-web/src/components/TopMenuBar.tsx
+++ life-manager-web/src/components/TopMenuBar.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { Navbar } from "react-bootstrap";
+import { Button, Form, FormControl, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 type Props = {
 
@@ -9,36 +10,28 @@ export const TopMenuBar: React.FC<Props> = props => {
     // const {} = props;
     // return <li>content</li>;
     return (
-        <nav className="navbar navbar-light">
-            <ul className="nav navbar-nav pull-xs-right">
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Life Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            </Nav>
+            <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+            </Form>
+        </Navbar.Collapse>
+        </Navbar>
 
-            <li className="nav-item">
-            <div className="nav-link">
-                Home
-            </div>
-            </li>
-
-            <li className="nav-item">
-            <div className="nav-link">
-                <i className="ion-compose"></i>&nbsp;New Post
-            </div>
-            </li>
-
-            <li className="nav-item">
-            <div className="nav-link">
-                <i className="ion-gear-a"></i>&nbsp;Settings
-            </div>
-            </li>
-
-            <li className="nav-item">
-            <div
-                className="nav-link">
-                <i className="ion-gear-a"></i>&nbsp;User
-            </div>
-            </li>
-
-            </ul>
-        </nav>
         // <Navbar bg="dark" variant="dark">
         //     <Navbar.Brand>LifeManager</Navbar.Brand>
         // </Navbar>
