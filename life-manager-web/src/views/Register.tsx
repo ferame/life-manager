d54aace6a154f0f429ddd7371c886303e5eb93b7
ferame <alisauskas.j@gmail.com>
2020-12-20 11:26:04 +0000

    Connection with backend is working, though it is just a register call currently

--- life-manager-web/src/views/Register.tsx
+++ life-manager-web/src/views/Register.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { useState } from 'react';
+import axios from "axios";
 // import { Counter } from '../features/counter/Counter';
 
 export default function Register () {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
 
     function handleSubmit (e:any) {
         e.preventDefault();
+        axios.post("/register", {
+            username: username,
+            password: password,
+            matchingPassword: password,
+            email: email
+        })
+        .then((response) => {
+            console.log("Response received");
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log("Error while performing the api call: '/register'");
+            console.log(error);
+        })
         console.log(username);
         console.log(password);
-        console.log("Eh");
     }
     // return <Counter/>;
     return (
@@ -27,6 +42,11 @@ export default function Register () {
                        name="password"
                        type="password"
                        placeholder="Password"/>
+                <input onChange={(event) => setEmail(event.target.value)}
+                       value={email}
+                       name="email"
+                       type="text"
+                       placeholder="Email"/>
                 <button type="submit">Create Account</button>
             </form>
         </div>
