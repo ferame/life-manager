77be6c0831144b4a736bcbb1a205e0d4181dfc30
ferame <alisauskas.j@gmail.com>
2020-12-26 22:27:14 +0000

    Work in progress. Added new, user specific endpoint for courses and integrating it to front end

--- life-manager-web/src/views/Home.tsx
+++ life-manager-web/src/views/Home.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../redux/reducers/userSlice';
+import axios from 'axios';
+
+interface User {
+    username: string;
+    token: string;
+}
+
+function getCourses(user: User) {
+    // profileAxios.interceptors.request.use(config => {
+    //     const token = user.token;
+    //     config.headers.Authorization = `Bearer ${token}`;
+    //     return config;
+    // })
+    axios.get('instructors/' + user.username + '/courses/assigned', {
+        headers: {
+            'Authorization': `Bearer ${user.token}`
+        }
+    })
+    .then(response => {
+        console.log('Response received');
+        console.log(response.data);
+        return response.data;
+    })
+    .catch(err => console.log(`Authentication error ${err}`));
+}
 
 export default function Home () {
     const user = useSelector(selectUser);
     console.log(user);
+    let courses = getCourses(user);
+    console.log(`Returned courses ${courses}`);
     return (
         <div>
             <h2>Current User</h2>
