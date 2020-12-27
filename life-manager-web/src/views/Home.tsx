2651a1daf6a34336c99d10231ea132a1af5beb13
ferame <alisauskas.j@gmail.com>
2020-12-27 23:20:37 +0000

    Adding additional courses endpoint

--- life-manager-web/src/views/Home.tsx
+++ life-manager-web/src/views/Home.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../redux/reducers/userSlice';
 import axios from 'axios';
+import { List, ListItem, ListItemText } from '@material-ui/core';
 
 interface User {
     username: string;
     token: string;
+}
+
+interface Course {
+    id: number;
+    username: string;
+    description: string;
 }
 
 function getCourses(user: User) {
@@ -14,29 +21,36 @@ function getCourses(user: User) {
     //     config.headers.Authorization = `Bearer ${token}`;
     //     return config;
     // })
-    axios.get('instructors/' + user.username + '/courses/assigned', {
+    return axios.get('instructors/' + user.username + '/courses/assigned', {
         headers: {
             'Authorization': `Bearer ${user.token}`
         }
     })
-    .then(response => {
-        console.log('Response received');
-        console.log(response.data);
-        return response.data;
-    })
+    .then(response => response.data)
     .catch(err => console.log(`Authentication error ${err}`));
 }
 
 export default function Home () {
     const user = useSelector(selectUser);
     console.log(user);
-    let courses = getCourses(user);
+    const [courses, setCourses] = useState([]);
+    getCourses(user);
     console.log(`Returned courses ${courses}`);
     return (
         <div>
             <h2>Current User</h2>
             <div>Username: {user.username}</div>
             <div>Token: {user.token}</div>
+            <div>
+                <List>
+                    {courses.map(function(item: Course, index: number){
+                        return (
+                        <ListItem>
+                            <ListItemText primary={item.description}/>
+                        </ListItem>)
+                    })}
+                </List>
+            </div>
         </div>
     )
 }
