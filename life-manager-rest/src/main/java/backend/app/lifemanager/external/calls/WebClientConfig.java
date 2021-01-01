4d2749101ffc692ffb51171840914c46f181a617
ferame <alisauskas.j@gmail.com>
2021-01-01 23:40:24 +0000

    Fixed performance issues of the Api calls from the backend by moving from Netty to Jetty

--- life-manager-rest/src/main/java/backend/app/lifemanager/external/calls/WebClientConfig.java
+++ life-manager-rest/src/main/java/backend/app/lifemanager/external/calls/WebClientConfig.java
@@ -1,27 +1,26 @@
 package backend.app.lifemanager.external.calls;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.eclipse.jetty.client.HttpClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.JettyClientHttpConnector;
+import org.springframework.http.client.reactive.JettyResourceFactory;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.TcpClient;
-
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
-    private TcpClient tcpClient = TcpClient
-            .create()
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-            .doOnConnected(connection -> {
-                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
-                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
-            });
 
-    public WebClient client = WebClient.builder()
-            .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
-            .build();
+    @Bean
+    public JettyResourceFactory resourceFactory() {
+        return new JettyResourceFactory();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        HttpClient httpClient = new HttpClient();
+        ClientHttpConnector connector = new JettyClientHttpConnector(httpClient, resourceFactory());
+
+        return WebClient.builder().clientConnector(connector).build();
+    }
 }
--- life-manager-rest/src/main/java/backend/app/lifemanager/features/weather/WeatherService.java
+++ life-manager-rest/src/main/java/backend/app/lifemanager/features/weather/WeatherService.java
@@ -1,39 +1,45 @@
 package backend.app.lifemanager.features.weather;
 
-import backend.app.lifemanager.external.calls.WebClientConfig;
 import backend.app.lifemanager.features.dao.WeatherForecast;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
 public class WeatherService {
-    private WebClientConfig webClientConfig;
+    @Autowired
+    private final WebClient webClient;
 
     @Value("${openweather.api.key}")
     private String apiKey;
+
+    @Value("${openweather.api.weather.authority}")
+    private String weatherAuthority;
 
     @Value("${openweather.api.weather.path}")
     private String weatherPath;
 
     @Autowired
-    public WeatherService(WebClientConfig webClientConfig) {
-        this.webClientConfig = webClientConfig;
+    public WeatherService(WebClient webClient) {
+        this.webClient = webClient;
     }
 
     public WeatherForecast getCurrent(String city) {
-        Mono<WeatherForecast> currentWeatherForecastMono = webClientConfig.client.get()
+        Mono<WeatherForecast> currentWeatherForecastMono = webClient.get()
                 .uri(uriBuilder -> uriBuilder
+                        .scheme("http")
+                        .host(weatherAuthority)
                         .path(weatherPath)
                         .queryParam("q", city)
                         .queryParam("appid", apiKey)
                         .build()
                 )
                 .retrieve().bodyToMono(WeatherForecast.class);
-        WeatherForecast currentWeatherForecast = currentWeatherForecastMono.block();
-        return currentWeatherForecast;
+//        TODO: add handling of the failing calls.
+        return currentWeatherForecastMono.block();
     }
 }
