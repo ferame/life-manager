2de65dfb63ce3791de89246f1ba7069eb53a7e5a
ferame <alisauskas.j@gmail.com>
2021-01-19 20:32:53 +0000

    Fixed issue with state updating in WeatherForecast component

--- life-manager-web/src/components/WeatherForecast.tsx
+++ life-manager-web/src/components/WeatherForecast.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from '../redux/reducers/userSlice';
+import { setLocation, setTemperature } from '../redux/reducers/weatherSlice';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Axios from 'axios';
@@ -12,8 +13,8 @@ const locations = [
 
 export default function WeatherForecast() {
     const user = useSelector(selectUser);
-    const [location, setLocation] = useState("");
-    const [temperature, setTemperature] = useState("");
+    const [location, setLoc] = useState("");
+    const [temperature, setTemp] = useState("");
     const dispatch = useDispatch();
 
     // TODO data fetching should be done as part of the redux action
@@ -23,11 +24,11 @@ export default function WeatherForecast() {
                 'Authorization': `Bearer ${user.token}`
             }
         }).then(response => {
-            setTemperature(response.data.main.temp);
+            setTemp(response.data.main.temp);
             dispatch(setTemperature(response.data.main.temp));
             dispatch(setLocation(location));
         });     
-    }, [user, location])
+    }, [user, location, dispatch])
 
     return (
         <div>
@@ -37,7 +38,7 @@ export default function WeatherForecast() {
                 id="location-selector"
                 value={location}
                 onChange={(event: any, newLocation: string | null) => {
-                    setLocation(newLocation === null ? "" : newLocation);
+                    setLoc(newLocation === null ? "" : newLocation);
                 }}
                 options={locations}
                 getOptionLabel={(option) => option.toUpperCase()}
