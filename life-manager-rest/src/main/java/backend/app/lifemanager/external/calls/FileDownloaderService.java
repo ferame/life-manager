a3746baaf7cf84c5f80888a5d25c7cada8ce9421
ferame <alisauskas.j@gmail.com>
2021-02-05 22:10:34 +0000

    Implementing the decompressing of downloaded locations gzip

--- life-manager-rest/src/main/java/backend/app/lifemanager/external/calls/FileDownloaderService.java
+++ life-manager-rest/src/main/java/backend/app/lifemanager/external/calls/FileDownloaderService.java
@@ -1,16 +1,19 @@
 package backend.app.lifemanager.external.calls;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.GZIPInputStream;
 
 @Slf4j
 @Service
@@ -22,12 +25,35 @@ public class FileDownloaderService {
     @Value("${openweather.api.locations.download.path}")
     private String downloadPath;
 
+    @Value("${openweather.api.locations.decompressed.path}")
+    private String decompressedPath;
+
     public void downloadFileFromUrlUsingNio() throws IOException {
         URL url = new URL(downloadUrl);
         ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-        FileOutputStream fos = new FileOutputStream(new File(downloadPath).getAbsolutePath());
+        String filePath = new File(downloadPath).getAbsolutePath();
+        FileOutputStream fos = new FileOutputStream(filePath);
         fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
         fos.close();
         rbc.close();
+        Path path = Paths.get(filePath);
+//        TODO: delete old json, if there is one.
+        decompressGzip(path);
+//        TODO: delete the gz file if decompressing is successful
+    }
+
+    private void decompressGzip(Path gzipPath) throws IOException {
+        try (
+                GZIPInputStream gis = new GZIPInputStream(new FileInputStream(gzipPath.toFile()));
+                FileOutputStream fos = new FileOutputStream(new File(decompressedPath).getAbsolutePath())
+        ) {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = gis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+        } catch (Exception exception) {
+            log.error("Failed to get gzip input stream of file output stream of decompressed file. Exception: " + exception.getMessage());
+        }
     }
 }
