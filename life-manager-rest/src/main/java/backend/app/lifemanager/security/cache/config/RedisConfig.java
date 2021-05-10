1a6b83bba834f37033471b80ed3039077340337c
ferame <alisauskas.j@gmail.com>
2021-05-10 21:56:56 +0100

    Seems like redis caching is fixed for docker, but front end is having communication issues with the backend.

--- life-manager-rest/src/main/java/backend/app/lifemanager/security/cache/config/RedisConfig.java
+++ life-manager-rest/src/main/java/backend/app/lifemanager/security/cache/config/RedisConfig.java
@@ -5,56 +5,79 @@ import backend.app.lifemanager.security.cache.queue.RedisMessagePublisher;
 import backend.app.lifemanager.security.cache.queue.RedisMessageSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 @Configuration
-@ComponentScan("backend.app.lifemanager.security.disabled.token")
-@EnableRedisRepositories(basePackages = "backend.app.lifemanager.security.cache.repo")
-@PropertySource("classpath:application.properties")
 public class RedisConfig {
-    @Value("${spring.redis.host}")
-    private String redisHost;
 
-    @Value("${spring.redis.port}")
-    private Integer redisPort;
+    @Value("${spring.redis.host}")
+    private String hostName;
+
+    @Value("${spring.redis.database}")
+    private int indexDataBase;
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
-        return new JedisConnectionFactory(config);
+    LettuceConnectionFactory lettuceConnectionFactory() {
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+
+        redisStandaloneConfiguration.setHostName(this.hostName);
+
+        redisStandaloneConfiguration.setDatabase(this.indexDataBase);
+
+        LettuceConnectionFactory lettuceConnectionFactory =
+                new LettuceConnectionFactory(redisStandaloneConfiguration);
+
+        return lettuceConnectionFactory;
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
+
         final RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+
+        template.setConnectionFactory(lettuceConnectionFactory());
+
+        template.setKeySerializer(new GenericToStringSerializer<>(Object.class));
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+
         return template;
     }
 
+    /**
+     * <b>MessageSubscriber</b> is  a implementation
+     * of {@link org.springframework.data.redis.connection.MessageListener}
+     * The MessageSubscriber is developed in the app
+     * return
+     */
     @Bean
     MessageListenerAdapter messageListener() {
         return new MessageListenerAdapter(new RedisMessageSubscriber());
     }
 
+
     @Bean
     RedisMessageListenerContainer redisContainer() {
+
         final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(jedisConnectionFactory());
+        container.setConnectionFactory(lettuceConnectionFactory());
         container.addMessageListener(messageListener(), topic());
         return container;
     }
 
+    /**
+     * <b>MessagePublisherImpl</b> is a implementation of @link redis.service.message.MessagePublisher
+     * The MessagePublisher is developed in the application
+     * return
+     */
     @Bean
     MessagePublisher redisPublisher() {
         return new RedisMessagePublisher(redisTemplate(), topic());
